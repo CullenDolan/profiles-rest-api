7405034937bdc8a87f456a5185169babef0d4161
cullen <cullen.dolan@gmail.com>
2021-03-01 09:21:57 -0600

    adding models:

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^_-j&fl*77&&sh8(dz!o-ppw-t!h7dclu-ev6$-nt^fr6m2ln8'
+# SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
