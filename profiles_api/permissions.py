fbeb63d023eecd6d1d2fc5e59b04a865f57983c4
cullen <cullen.dolan@gmail.com>
2021-03-11 09:18:17 -0600

    changed authetication to sign in only to view feed

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -11,3 +11,13 @@ class UpdateOwnProfile(permissions.BasePermission):
     
         return obj.id == request.user.id # will return true if user is trying to update their own profile
 
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update pwn status"""
+    def has_object_permission(self, request, view, obj):
+        """chcek the user is trying to update their own statys"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_profile.id == request.user.id
+        
